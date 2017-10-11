cab54a6d4ed0d712a0d6a477b26892bfca1b3f6d
jonahpelfrey <jsp916@gmail.com>
2017-10-11 16:06:42 -0500

    first commit

--- tracker/ViewController.swift
+++ tracker/ViewController.swift
@@ -8,18 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //VARS
+    var proteinGoal: Int = 0
+    var calorieGoal: Int = 0
+    var carbGoal: Int = 0
+    
+    
+    //OUTLETS
 
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var gramsOfProtein: UILabel!
+    @IBOutlet weak var proteinProgressBar: UIProgressView!
+    
+    @IBOutlet weak var numberOfCalories: UILabel!
+    @IBOutlet weak var calorieProgressBar: UIProgressView!
+    
+    @IBOutlet weak var numberOfCarbs: UILabel!
+    @IBOutlet weak var carbProgressBar: UIProgressView!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.proteinProgressBar.progress = 0
+        self.carbProgressBar.progress = 0
+        self.calorieProgressBar.progress = 0
+        
+        
+//        self.proteinProgressBar.animate(duration: 10)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //FUNCTIONS
+    func retrieveProgress() {
+        
     }
-
+    
+    func retrieveGoals() {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
 
 }
+
+extension UIProgressView {
+    
+    func animate(duration: Double) {
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: .curveLinear, animations: {
+            self.setProgress(1.0, animated: true)
+        }, completion: nil)
+    }
+}
+
+
+
 
