decb9cfa91ba645016766ad45e1eeede961dc8f7
jonahpelfrey <jsp916@gmail.com>
2017-10-18 18:19:18 -0500

    update values based on view input

--- tracker/ViewController.swift
+++ tracker/ViewController.swift
@@ -10,61 +10,88 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    //VARS
+    //CLASS
     var proteinGoal: Int = 0
     var calorieGoal: Int = 0
     var carbGoal: Int = 0
     
+    var textFields = [UITextField]()
     
     //OUTLETS
-    @IBOutlet weak var proteinTextField: UITextField!
-    @IBOutlet weak var caloriesTextField: UITextField!
-    @IBOutlet weak var carbsTextField: UITextField!
-    @IBOutlet weak var fatTextField: UITextField!
-    @IBOutlet weak var sugarTextField: UITextField!
-    
     @IBOutlet weak var popupCenterConstraint: NSLayoutConstraint!
     @IBOutlet weak var dateLabel: UILabel!
     
     @IBOutlet weak var gramsOfProtein: UILabel!
     @IBOutlet weak var proteinProgressBar: UIProgressView!
+    @IBOutlet weak var proteinTextField: UITextField!
     
     @IBOutlet weak var numberOfCalories: UILabel!
     @IBOutlet weak var calorieProgressBar: UIProgressView!
+     @IBOutlet weak var caloriesTextField: UITextField!
     
     @IBOutlet weak var numberOfCarbs: UILabel!
     @IBOutlet weak var carbProgressBar: UIProgressView!
+    @IBOutlet weak var carbsTextField: UITextField!
     
     @IBOutlet weak var numberOfFats: UILabel!
     @IBOutlet weak var fatProgressBar: UIProgressView!
+    @IBOutlet weak var fatTextField: UITextField!
     
     @IBOutlet weak var numberOfSugars: UILabel!
     @IBOutlet weak var sugarProgressBar: UIProgressView!
+    @IBOutlet weak var sugarTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.proteinProgressBar.progress = 0
-        self.carbProgressBar.progress = 0
-        self.calorieProgressBar.progress = 0
-        //self.proteinProgressBar.animate(duration: 10)
         
-        
-        
+        setupProgressBars()
     }
     
     
     //FUNCTIONS
     @IBAction func addNutrients(_ sender: Any) {
-        self.popupCenterConstraint.constant = -400
         
-        UIView.animate(withDuration: 0.2, animations: {
-            self.view.layoutIfNeeded()
-        })
+        if let pr = proteinTextField.text, !pr.isEmpty {
+            let newProtein = Int(pr)
+            if let oldProtein = Int(gramsOfProtein.text!) {
+                let protein = newProtein! + oldProtein
+                gramsOfProtein.text = "\(protein)"
+            }
+        }
         
-        //Save numbers
+        if let cal = caloriesTextField.text, !cal.isEmpty {
+            let newCalories = Int(cal)
+            if let oldCalories = Int(numberOfCalories.text!) {
+                let calories = newCalories! + oldCalories
+                numberOfCalories.text = "\(calories)"
+            }
+        }
         
-        //Reset text fields
+        if let carb = carbsTextField.text, !carb.isEmpty {
+            let newCarbs = Int(carb)
+            if let oldCarbs = Int(numberOfCarbs.text!) {
+                let carbs = newCarbs! + oldCarbs
+                numberOfCarbs.text = "\(carbs)"
+            }
+        }
         
+        if let sugar = sugarTextField.text, !sugar.isEmpty {
+            let newSugar = Int(sugar)
+            if let oldSugar = Int(numberOfSugars.text!) {
+                let sugars = newSugar! + oldSugar
+                numberOfSugars.text = "\(sugars)"
+            }
+        }
+        
+        if let fat = fatTextField.text, !fat.isEmpty {
+            let newFat = Int(fat)
+            if let oldFat = Int(numberOfFats.text!) {
+                let fats = newFat! + oldFat
+                numberOfFats.text = "\(fats)"
+            }
+        }
+        animateOut()
+        clearFields()
     }
     
     @IBAction func addNewEntry(_ sender: Any) {
@@ -83,6 +110,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
         return true
+    }
+    
+    func animateOut() {
+        
+        self.popupCenterConstraint.constant = -400
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func clearFields() {
+        proteinTextField.text = ""
+        caloriesTextField.text = ""
+        sugarTextField.text = ""
+        fatTextField.text = ""
+        carbsTextField.text = ""
+    }
+    
+    func setupProgressBars() {
+        
+        //Check user defaults
+        //self.proteinProgressBar.animate(duration: 10)
+        
+        //else
+        self.proteinProgressBar.progress = 0
+        self.carbProgressBar.progress = 0
+        self.calorieProgressBar.progress = 0
+        self.fatProgressBar.progress = 0
+        self.sugarProgressBar.progress = 0
+        
     }
     
 
